a3a6e5f4ad077f8276b079fa7f74084991352eac
yehudisr <77118499+yehudisr@users.noreply.github.com>
2021-04-28 11:31:44 -0400

    commit tree d12f8d87998491ab23bc7d7be30a5ff484e2b231

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 import counterReducer from "./features/counter/counterSlice.js";
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(counterReducer, 
+     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, document.getElementById("root"));
